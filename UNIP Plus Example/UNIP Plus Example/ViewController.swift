9e1672d76509ca1d827a12b71cf11787d1961d6c
José Franklin Silva Alves <josealves@MBP-de-Jose-Alves.local>
2021-11-05 19:41:44 -0300

    UNIP Plus Example project setup

---
+++ UNIP Plus Example/UNIP Plus Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UNIP Plus Example
+//
+//  Created by José Franklin Silva Alves on 05/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
